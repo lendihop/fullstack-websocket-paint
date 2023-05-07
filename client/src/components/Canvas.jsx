0fea972231281b81eb5e9a3219402e545955b675
lendihop <mishin.danil2017@gmail.com>
2023-05-07 23:55:23 +0200

    brush + rect tools

--- client/src/components/Canvas.jsx
+++ client/src/components/Canvas.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import '../styles/canvas.scss';
+import {observer} from "mobx-react-lite";
+import canvasState from "../store/canvasState";
+import toolState from "../store/toolState";
+import Brush from "../tools/Brush";
 
-const Canvas = () => {
+const Canvas = observer(() => {
+    const canvasRef = useRef();
+
+    useEffect(() => {
+        canvasState.setCanvas(canvasRef.current);
+        toolState.setTool(new Brush(canvasRef.current));
+    }, []);
+
     return (
         <div className='canvas'>
-            <canvas width={900} height={600}></canvas>
+            <canvas ref={canvasRef} width={900} height={600}></canvas>
         </div>
     );
-};
+});
 
 export default Canvas;
--- client/src/components/Toolbar.jsx
+++ client/src/components/Toolbar.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import '../styles/toolbar.scss';
+import toolState from "../store/toolState";
+import Brush from "../tools/Brush";
+import canvasState from "../store/canvasState";
+import Rect from "../tools/Rect";
 
 const Toolbar = () => {
     return (
         <div className='toolbar'>
-            <button className='toolbar__btn brush'/>
-            <button className='toolbar__btn rect'/>
+            <button className='toolbar__btn brush' onClick={() => toolState.setTool(new Brush(canvasState.canvas))}/>
+            <button className='toolbar__btn rect' onClick={() => toolState.setTool(new Rect(canvasState.canvas))}/>
             <button className='toolbar__btn circle'/>
             <button className='toolbar__btn eraser'/>
             <button className='toolbar__btn line'/>
