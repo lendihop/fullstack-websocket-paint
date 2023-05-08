3f004992205f084002cc475f355b63004482b144
lendihop <mishin.danil2017@gmail.com>
2023-05-08 12:04:19 +0200

    rest tools + settings

--- client/src/components/SettingsBar.jsx
+++ client/src/components/SettingsBar.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import '../styles/toolbar.scss';
+import toolState from "../store/toolState";
 
 const SettingsBar = () => {
     return (
         <div className='setting-bar'>
+            <label style={{marginLeft: 10}} htmlFor='line-width'>Line width:</label>
+            <input
+                onChange={e => toolState.setLineWidth(e.target.value)}
+                style={{margin: '0 10px'}}
+                id='line-width'
+                type='number'
+                defaultValue={1} min={1} max={50}/>
 
+            <label style={{marginLeft: 10}} htmlFor='stroke-color'>Stroke color:</label>
+            <input
+                onChange={e => toolState.setStrokeColor(e.target.value)}
+                style={{margin: '0 10px'}}
+                id='stroke-color'
+                type='color'/>
         </div>
     );
 };
--- client/src/components/Toolbar.jsx
+++ client/src/components/Toolbar.jsx
@@ -4,16 +4,24 @@ import toolState from "../store/toolState";
 import Brush from "../tools/Brush";
 import canvasState from "../store/canvasState";
 import Rect from "../tools/Rect";
+import Circle from "../tools/Circle";
+import Line from "../tools/Line";
+import Eraser from "../tools/Eraser";
 
 const Toolbar = () => {
+    const changeColor = e => {
+        toolState.setFillColor(e.target.value);
+        toolState.setStrokeColor(e.target.value);
+    }
+
     return (
         <div className='toolbar'>
             <button className='toolbar__btn brush' onClick={() => toolState.setTool(new Brush(canvasState.canvas))}/>
             <button className='toolbar__btn rect' onClick={() => toolState.setTool(new Rect(canvasState.canvas))}/>
-            <button className='toolbar__btn circle'/>
-            <button className='toolbar__btn eraser'/>
-            <button className='toolbar__btn line'/>
-            <input style={{marginLeft: 10}} type="color"/>
+            <button className='toolbar__btn circle' onClick={() => toolState.setTool(new Circle(canvasState.canvas))}/>
+            <button className='toolbar__btn eraser' onClick={() => toolState.setTool(new Eraser(canvasState.canvas))}/>
+            <button className='toolbar__btn line' onClick={() => toolState.setTool(new Line(canvasState.canvas))}/>
+            <input onChange={changeColor} style={{marginLeft: 10}} type='color'/>
             <button className='toolbar__btn undo'/>
             <button className='toolbar__btn redo'/>
             <button className='toolbar__btn save'/>
